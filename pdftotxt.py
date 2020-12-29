2c9cadb8b87616e28a22058f5155762fe8ce257a
cyliesho <colinvanlieshout@gmail.com>
2020-12-29 11:02:21 +0100

    cleaned up pdftotxt

--- pdftotxt.py
+++ pdftotxt.py
@@ -14,9 +14,7 @@ observations Colin:
 
 todo:
 - imrpove conversion
-- send output to transformer
 - catch large files
-
 """
 
 
@@ -35,21 +33,6 @@ def convertpdftotxt(filename, path_txt, path_pdf = None, overwrite = False):
             filepath = filename
         convertapi.api_secret = 'GQohrMpAMRIVg4JS'  #QKLmVCcVJHkpZ8AN
         convertapi.convert('txt', {'File': filepath}, from_format = 'pdf').save_files(path_txt)
-        # convertedfile = convertapi.convert('txt', {'File': filepath}, from_format = 'pdf').save_files(path_txt)
-        # convertedfile = str(convertedfile[0])
-
-    # with open(convertedfile, encoding='utf8') as filehandle:
-    #     lines = filehandle.read() #use read instead of readlines
-    #     sent_split = nltk.sent_tokenize(lines)
-
-
-    #with open(convertedfile, 'w', encoding='utf8') as filehandle:
-        #lines = filter(lambda x: x.strip(), lines)
-        #filehandle.writelines(lines)
-        #txt = 
-        #print(lines)
-        #sent_split = sent_tokenize(lines)
-    # return sent_split  
 
 def check_if_txt_exists(path_txt, filename):
     """
@@ -64,29 +47,6 @@ def check_if_txt_exists(path_txt, filename):
     else:
         print('File does not yet exist, is now converted and stored')
         return False
-
-
-
-
-
-
-
-### Main script ###
-
-# path_pdf= 'data/pdf'
-# path_txt = 'data/txt'
-# filename = 'tk-besluit-op-bezwaar-op-grond-van-de-wob-inzake-poch-en-stand-van-zaken-onderzoek-commissie-dossier-j-a-poch.pdf'
-# pdffile = 'data/pdf/tk-bijlage-scan-geanonimiseerd-besluit-en-inventarislijst-wob-zelfonderzoek-door-advocaten.pdf'
-
-# check_if_txt_exists(path_pdf, path_txt, filename)
-
-# convertedfilename = convertpdftotxt(filename, path_txt, path_pdf) #returns list of tokenized sentences
-
-# print(convertedfilename)
-
-# print(convertedfilename)
-
-# convertedfilename.write('test.txt')
 
 
 
